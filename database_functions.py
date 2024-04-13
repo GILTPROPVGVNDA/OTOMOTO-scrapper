4484d6abd7728d3ab87bbf713ba81fa502dbc4ce
GILTPROPVGVNDA <Bprysak13@gmail.com>
2024-04-13 19:14:14 +0200

    added requirements.txt and small changes in code

--- database_functions.py
+++ database_functions.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 
+
 def save_to_mongodb(data):
     """This function saves a dictionary to MongoDB database"""
-    client = MongoClient('mongodb+srv://molakrzysztof:<tu wpisz hasło do bazy>@cars.uwewuw1.mongodb.net/?retryWrites=true&w=majority&appName=Cars')
+    client = MongoClient(
+        'mongodb+srv://molakrzysztof:papiez69xD*@cars.uwewuw1.mongodb.net/?retryWrites=true&w=majority&appName=Cars')
 
     db = client["car_info"]
     collection = db["cars_info"]
